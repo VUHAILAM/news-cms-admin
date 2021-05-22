a8aed9156ff9cffc380051e0ccfb1b1fb18b820e
Manh Pham <v.manhpt1@vinbigdata.org>
2021-05-22 17:10:03 +0700

    prepare layout

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,57 @@
 <template>
   <v-app>
+    <v-navigation-drawer app permanent expand-on-hover>
+      <v-list>
+        <!--        <v-list-item class="px-2">-->
+        <!--          <v-list-item-avatar>-->
+        <!--            <v-img-->
+        <!--              src="https://randomuser.me/api/portraits/women/85.jpg"-->
+        <!--            ></v-img>-->
+        <!--          </v-list-item-avatar>-->
+        <!--        </v-list-item>-->
+
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="title">Admin</v-list-item-title>
+            <v-list-item-subtitle>v.admin@vinbigdata.org</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list nav dense>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-newspaper-variant-multiple</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Posts</v-list-item-title>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-spider-thread</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Crawler</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
-          alt="Vuetify Logo"
+          alt="VinBase Logo"
           class="shrink mr-2"
           contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          :src="require('./assets/logo-vinbase.svg')"
           transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
         />
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <router-view />
+      <v-container fluid>
+        <router-view />
+      </v-container>
     </v-main>
   </v-app>
 </template>
